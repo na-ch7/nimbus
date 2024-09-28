e814792450af220488a86d98b767a0b107265629
na-ch7 <141823845+na-ch7@users.noreply.github.com>
2024-09-29 02:39:16 +0530

    feat: setup server

---
+++ server/src/shared/errors.ts
@@ -0,0 +1,9 @@
+export const ERRORS = {
+  INTERNAL_SERVER_ERROR: {
+    statusCode: 500,
+    message: {
+      error: 'Internal Server Error',
+      error_description: 'Something went wrong',
+    },
+  },
+};
